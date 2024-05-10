88ca9a2d7f3307f670b2f5d09886d1e94c283f1e
404background <404background@gmail.com>
2024-05-10 19:25:52 +0900

    Fix programs

--- reazonspeech.js
+++ reazonspeech.js
@@ -1,23 +1,21 @@
 module.exports = function(RED) {
-    function Pip(config) {
+    function ReazonSpeech(config) {
         RED.nodes.createNode(this,config)
         let node = this
-        let argument = ''
-        let action = ''
         let command = ''
         const path = require('path')
         const fs = require('fs')
-        const jsonPath = path.join(path.dirname(__dirname), 'path.json')
+        const jsonPath = path.join(__dirname, 'path.json')
         const json = fs.readFileSync(jsonPath)
 
         const pythonPath = JSON.parse(json).NODE_PYENV_PYTHON
         const execSync = require('child_process').execSync
 
         node.on('input', function(msg) {
-            command = pythonPath + ' ' + 'reazonspeech-nemo-asr ' + config.voicePath
+            command = path.dirname(pythonPath) + '/reazonspeech-nemo-asr ' + config.voicePath
             msg.payload = String(execSync(command))
             node.send(msg)
         })
     }
-    RED.nodes.registerType('reazonspeech',Pip)
+    RED.nodes.registerType('reazonspeech',ReazonSpeech)
 }
